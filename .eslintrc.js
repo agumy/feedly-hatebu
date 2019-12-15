e55821c25ddc0400d0f0c71aedec20cefab5dc6e
yuni3 <yuni3.dev@gmail.com>
2019-12-15 22:05:49 +0900

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {},
+}
